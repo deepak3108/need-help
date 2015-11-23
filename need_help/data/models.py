d24bfcddf1bbbd1cabb6c455e6c5497509a4726b
deepak3108 <deepakbhimavarapu@gmail.com>
2015-11-23 17:34:30 +0530

    database created

--- need_help/data/models.py
+++ need_help/data/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Database(models.Model):
+class Donor(models.Model):
 	donar_name = models.CharField(max_length = 25)
-	mobile_number = models.BigIntegerField()
 	blood_group = models.CharField(max_length = 20)
-	city_name = models.CharField(max_length = 20)	
+	city_name = models.CharField(max_length = 20)
+	mobile_number = models.BigIntegerField()
+	password = models.CharField(max_length = 20)
+
+	
